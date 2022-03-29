38807a18c4e67aff6dff3ea7b1268330f1192862
aidanliddiard <aidanliddiard@gmail.com>
2022-03-29 15:18:02 -0700

    dog detail page working

--- src/Views/DogDetail.js
+++ src/Views/DogDetail.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchDogById } from '../services/dogs';
 
 export default function DogDetail() {
   const params = useParams();
-  console.log(params);
+
+  const [dog, setDog] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchDogById(params.id);
-      console.log(data);
+      setDog(data);
     };
     fetchData();
   }, [params.id]);
 
-  return <div>DogDetail</div>;
+  return (
+    <div>
+      <h2>{dog.name}</h2>
+      <p>
+        Hi! I am {dog.name}!
+        <ul>
+          <li>Age: {dog.age}</li>
+          <li>Breed: {dog.breed}</li>
+        </ul>
+      </p>
+      <img width="350px" src={dog.image} />
+      <p>My trainers say I am `{dog.bio}`</p>
+    </div>
+  );
 }
